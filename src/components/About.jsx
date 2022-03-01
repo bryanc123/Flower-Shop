9cd36c3ea64af8caa0944849caa5aaac40a01357
Bryan Cayanan <cayanan.bryanau@gmail.com>
2022-03-01 02:22:08 -0800

    Updated the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,17 @@
 const About = () => {
     return (
-        <p>This is the about page.</p>
+        <section className="about">
+            <div className="about-container">
+                <img src="images/bouquet.jpg" alt="Bouquet" className="about__image" />
+                <h2 className="about__heading">
+                    <span className="about__heading--fragment-one">Personalize </span>
+                    <span className="about__heading--fragment-two">Your </span>
+                    <span className="about__heading--fragment-three">Bouquet</span>
+                </h2>
+                <p>Founded in 2022, Flower Shop offers home-grown flowers for any occasion</p>
+            </div>
+            <p>Photo by <a href="https://pixnio.com/author/milim84">Marko Milivojevic</a></p>
+        </section>
     )
 };
 
