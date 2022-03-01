035fda179153bfe93cb1da7d81f2c85b40ebacb2
Bryan Cayanan <cayanan.bryanau@gmail.com>
2022-03-01 03:09:05 -0800

    Updated the product page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,38 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import data from '../data';
 
 const Product = () => {
+    let [quantity, setQuantity] = useState(0);
+
     let { name } = useParams();
-
     let product = data.find(item => item.name === name);
-
     let photographer = product.photographer ? `Photo by ${product.photographer}` : '';
+
+    const onDecrement = (event) => {
+        setQuantity(previousQuantity => {
+            if(previousQuantity > 0) {
+                return previousQuantity - 1;
+            }
+
+            return 0;
+        })
+    }
+
+    const onIncrement = (event) => {
+        setQuantity(previousQuantity => {
+            if(previousQuantity < product.quantity) {
+                return previousQuantity + 1;
+            }
+
+            return previousQuantity;
+        })
+    }
+
+    const onChange = (event) => {
+        setQuantity(event.target.value);
+    };
 
     return (
         <>
@@ -19,6 +44,12 @@ const Product = () => {
                 <div className="product__description-container">
                     <h2>{product.description}</h2>
                     <p>Price: ${product.price}</p>
+                    <p>In Stock: {product.quantity}</p>
+                    <span>Quantity:</span>
+                    <button className="product__decrement" onClick={onDecrement}>-</button>
+                    <input type="text" value={quantity} onChange={onChange} className="product__quantity"></input>
+                    <button className="product__increment" onClick={onIncrement}>+</button>
+                    <button className="product__add">Add to Cart</button>
                 </div>
             </section>
         </>
