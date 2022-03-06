e0bed44b294059df68be58e2f4fb3431752883b9
Bryan Cayanan <cayanan.bryanau@gmail.com>
2022-03-05 17:23:44 -0800

    Added scroll restoration

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import ScrollToTop from './components/ScrollToTop';
 import Layout from './components/Layout';
 import './App.css';
 
@@ -6,6 +7,7 @@ import { HashRouter as Router } from 'react-router-dom';
 function App() {
   return (
     <Router basename="/" hashType="slash">
+      <ScrollToTop />
       <div className="App">
         <Layout />
       </div>
