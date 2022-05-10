39a1afcf7754b02043e7282f26e8a4d13a2cf402
Bryan Cayanan <cayanan.bryanau@gmail.com>
2022-05-10 12:53:57 -0700

    Added recommended products section

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { addToCart, updateProductQuantity, setCartUpdated } from '../features/cart/cartSlice';
 
@@ -94,26 +95,68 @@ const Product = () => {
         dispatch(setCartUpdated(true));
     };
 
+    let recommendedProducts = [];
+    for(let i = 0; i < data.length; i++) {
+        if(data[i].name === name) {
+            continue;
+        }
+        let ratingData = ratingsData.find(rating => rating.name === data[i].name);
+        let numberOfRatings = ratingData.ratings.length;
+        let rating = ratingData.ratings.reduce((previous, current) => { return previous + current }) / numberOfRatings;
+        rating = Math.round(rating * 10) / 10;
+        recommendedProducts.push({
+            ...data[i],
+            rating,
+            numberOfRatings
+        });
+        if(recommendedProducts.length === 3) {
+            console.log(recommendedProducts);
+            break;
+        }
+    }
+    recommendedProducts = recommendedProducts.map(product => {
+        return (
+            <div className="recommended-product" key={product.name}>
+                <Link to={`/product/${product.name}`}>
+                    <img src={`images/${name}.jpg`} alt={product.description} className="recommended-product__image" />
+                </Link>
+                <Link to={`/product/${product.name}`}>
+                    <h3>{product.description}</h3>
+                </Link>
+                <p>Price: ${product.price}</p>
+                <p>Rating: {product.rating} / 5 ({product.numberOfRatings} ratings)</p>
+                <p>In Stock: {product.quantity}</p>
+            </div>
+        )
+    });
+
     return (
         <section className="product">
-            <div className="product-container">
-                <div className="product__images-container">
-                    <img src={`images/${name}.jpg`} alt="Tulips" className="product__image" />
-                    <p>{photographer}</p>
+            <div className="product__container">
+                <div className="product__purchase-container">
+                    <div className="product__images-container">
+                        <img src={`images/${name}.jpg`} alt={product.description} className="product__image" />
+                        <p>{photographer}</p>
+                    </div>
+                    <div className="product__description-container">
+                        <h2>{product.description}</h2>
+                        <p>Price: ${product.price}</p>
+                        <p>Rating: {rating} / 5 ({numberOfRatings} ratings)</p>
+                        <p>In Stock: {product.quantity}</p>
+                        <span>Quantity:</span>
+                        <button className="product__decrement" onClick={onDecrement}>-</button>
+                        <input type="text" value={quantity} onChange={onChange} className="product__quantity"></input>
+                        <button className="product__increment" onClick={onIncrement}>+</button>
+                        <button className="product__add" onClick={add}>Add to Cart</button>
+                        {error && <div className="product__error-message">{error}</div>}
+                        {productAdded && <div className="product__added-message">Product added to cart</div>}
+                    </div>
                 </div>
-                <div className="product__description-container">
-                    <h2>{product.description}</h2>
-                    <p>Price: ${product.price}</p>
-                    <p>Rating: {rating} / 5 ({numberOfRatings} ratings)</p>
-                    <p>In Stock: {product.quantity}</p>
-                    <span>Quantity:</span>
-                    <button className="product__decrement" onClick={onDecrement}>-</button>
-                    <input type="text" value={quantity} onChange={onChange} className="product__quantity"></input>
-                    <button className="product__increment" onClick={onIncrement}>+</button>
-                    {/* <button className="product__add" onClick={addToCart}>Add to Cart</button> */}
-                    <button className="product__add" onClick={add}>Add to Cart</button>
-                    {error && <div className="product__error-message">{error}</div>}
-                    {productAdded && <div className="product__added-message">Product added to cart</div>}
+                <div className="recommended-products">
+                    <h2>Recommended Products</h2>
+                    <div className="recommended-products__row">
+                        {recommendedProducts}
+                    </div>
                 </div>
             </div>
         </section>
