80881474c7bcda05d90a347dfb56ce16a5df9785
Bryan Cayanan <cayanan.bryanau@gmail.com>
2022-05-12 15:52:55 -0700

    Added GraphQL and Apollo Client to fetch sample data for customer reviews

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,32 @@
+import { useQuery, gql } from "@apollo/client";
+
 const About = () => {
+
+    const FETCH_REVIEWS = gql`
+        query FetchReviews (
+            $options: PageQueryOptions
+        ) {
+            posts(options: $options) {
+                data {
+                    id,
+                    title,
+                    body
+                }
+            }
+        }
+    `;
+
+    const { loading, error, data } = useQuery(FETCH_REVIEWS, {
+        variables: {
+            options: {
+                paginate: {
+                    page: 1,
+                    limit: 5
+                }
+            }
+        }
+    });
+
     return (
         <section className="about">
             <div className="about__container">
@@ -15,7 +43,7 @@ const About = () => {
                 </div>
                 <div className="about__text-container">
                     <h2>About Flower Shop</h2>
-                    <p>Founded in 2022, Flower Shop offers home-grown flowers for any occasion</p>
+                    <p>Founded in 2022, Flower Shop offers homegrown flowers for any occasion</p>
                     <p>You decide what flowers go into your personal bouquets.</p>
                     <p>Our shop offers:</p>
                     <ul>
@@ -29,6 +57,20 @@ const About = () => {
                     </ul>
                     <p>All of our products come at affordable prices.</p>
                 </div>
+            </div>
+            <div className="customer-reviews__container">
+                <h2>Customer Reviews</h2>
+                {
+                    loading ?
+                        <p>Loading Reviews...</p> :
+                        error ? <p>Error</p> :
+                            data.posts.data.map(({ id, title, body }) => (
+                                <div className="customer-review" key={id}>
+                                    <h3 style={{margin:"0"}}>{title}</h3>
+                                    <p>{body}</p>
+                                </div>
+                            ))
+                }
             </div>
         </section>
     )
--- src/index.js
+++ src/index.js
@@ -4,16 +4,28 @@ import './index.css';
 import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from '@apollo/client';
 
 const container = document.getElementById('root');
 
 const root = ReactDOM.createRoot(container);
 
+const client = new ApolloClient({
+  uri: 'https://graphqlzero.almansi.me/api',
+  cache: new InMemoryCache()
+})
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
