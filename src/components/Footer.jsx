dc255ef8c1a536ec057e3ceeb3d42e8f375f5297
Bryan Cayanan <cayanan.bryanau@gmail.com>
2022-05-18 11:36:13 -0700

    Updated styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 const Footer = () => (
     <footer>
-        <div className="footer-container">
+        <div className="footer__container">
             <div className="footer__section">
                 <h2>Links</h2>
                 <ul>
+                    <li><a href="https://github.com/bryanc123/Flower-Shop">Source Code</a></li>
                     <li><a href="https://bryanc123.github.io/Personal-Website/">Personal Portfolio</a></li>
                     <li><a href="https://github.com/bryanc123">GitHub</a></li>
                 </ul>
@@ -11,7 +12,7 @@ const Footer = () => (
             <div className="footer__section">
                 <h2>Other Projects</h2>
                 <ul>
-                    <li><a href="https://bryanc123.github.io/Employee-Records-Angular/">User Records Angular App</a></li>
+                    <li><a href="https://bryanc123.github.io/Employee-Records-Angular/">Employee Records</a></li>
                     <li><a href="https://bryanc123.github.io/Mobile-First-Design/">Mobile First Design</a></li>
                     <li><a href="https://bryanc123.github.io/Quiz-Game/">Quiz Game</a></li>
                 </ul>
@@ -20,7 +21,7 @@ const Footer = () => (
                 <h2>Contact</h2>
                 <ul>
                     <li><strong>Phone Number:</strong> 1-805-608-0066</li>
-                    <li><strong>E-mail:</strong> cayanan.bryanau@gmail.com</li>
+                    <li><strong>Email:</strong> cayanan.bryanau@gmail.com</li>
                 </ul>
             </div>
         </div>
