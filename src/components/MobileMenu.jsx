e4dedea2e689e780a0da76203fa619eae604d8ba
Bryan Cayanan <cayanan.bryanau@gmail.com>
2022-06-10 16:18:34 -0700

    Updated formatting of code

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const MobileMenu = () => {
-
     const cartUpdated = useSelector((state) => state.cart.updated);
 
     const closeMobileMenu = () => {
