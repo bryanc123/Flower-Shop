3c2889d0077d9d4dfc806ca98db04078e85516ec
Bryan Cayanan <cayanan.bryanau@gmail.com>
2023-07-14 19:07:00 -0700

    Added route to Login component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,6 +4,7 @@ import Footer from './Footer';
 import ProductList from './ProductList';
 import Product from './Product';
 import About from './About';
+import Login from './Login';
 import Settings from './Settings';
 import Cart from './Cart';
 import MobileMenu from './MobileMenu';
@@ -19,6 +20,7 @@ const Layout = () => {
                 <Route path="/" element={<ProductList />} />
                 <Route path="/product/:name" element={<Product />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="/settings" element={<Settings />} />
                 <Route path="/cart" element={<Cart />} />
             </Routes>
